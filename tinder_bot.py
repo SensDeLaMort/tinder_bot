a738abe6af4d4922e76097ab5483745e066e2800
SensDeLaMort <mr.r.imanov@gmail.com>
2020-03-25 01:53:03 +0400

    first commit

---
+++ tinder_bot.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from time import sleep
+from random import randint
+
+class TinderBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def login(self):
+        self.driver.get('https://tinder.com')
+
+        isLogged = False
+        print('Write "y" if you logged in to your account')
+
+        while not isLogged:
+            answer = input()
+            if answer.lower() == 'y':
+                isLogged = True
+            else:
+                print('Wrong answer. Try again')
+
+        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_1.click()
+
+        popup_2 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_2.click()
+
+    def like(self):
+        like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/div/main/div/div[1]/div/div[2]/div[4]/button')
+        like_btn.click()
+
+    def dislike(self):
+        dislike_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/div/main/div/div[1]/div/div[2]/div[2]/button')
+        dislike_btn.click()
+
+    def auto_swipe(self):
+        while True:
+            sleep(0.5)
+            try:
+                rand = randint(1, 100)
+                if rand <= 73:
+                    self.like()
+                else:
+                    self.dislike()
+            except Exception:
+                try:
+                    self.close_popup()
+                except Exception:
+                    self.close_match()
+
+    def close_popup(self):
+        popup_3 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/button[2]')
+        popup_3.click()
+
+    def close_match(self):
+        match_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
+        match_popup.click()
+
+
+bot = TinderBot()
+bot.login()
+bot.auto_swipe()
